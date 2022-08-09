2a87ba783e23d903d762785ecbe5b1fa2948359d
Sudeep <sudeep7217@gmail.com>
2022-08-09 22:33:20 +0530

    Pointer Animated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
@@ -17,6 +18,17 @@ function App() {
       <Contact />
 
       <SocialLinks />
+      <AnimatedCursor 
+      color="255,255,255"
+      innerSize={8}
+      outerSize={35}
+      innerScale={1}
+      outerScale={0}//1.7
+      outerAlpha={0}
+      outerStyle={{
+        border: '3px solid #fff'
+      }}
+      />
     </div>
   );
 }
