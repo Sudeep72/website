277d543ececf4a84b4a2a23a217751b3a0b382fe
Sudeep <sudeep7217@gmail.com>
2022-08-12 21:41:24 +0530

    Button change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,12 +22,12 @@ const Home = () => {
 
           <div>
             <Link
-              to="portfolio"
+              to="contact"
               smooth
               duration={500}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
-              Portfolio
+              Hire Me
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
